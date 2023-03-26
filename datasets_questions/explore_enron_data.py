45d608d669ca94ba0eb62b80cce6d85d9133a1e0
murshidomer <murshidomer@outlook.com>
2023-03-26 22:58:40 +0400

    Add/update solutions: explore_enron_data.py, datasetsAndQuestions.ipynb

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -14,6 +14,8 @@
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
     
 """
+import re
+from time import time
 
 import joblib
 
@@ -30,19 +32,40 @@ for data in enron_data:
 print(count)
 
 # POIs in enron data
+# not doing anything witH this list, unnecessary
+# poicount = [enron_data["name"]for data in enron_data if enron_data[data]["poi"] == 1]
+# print(len(poicount))
+# next(item for item in enron_data if enron_data[item]["poi"] == "1")
 poiCount = 0;
 for data in enron_data:
-    for key in enron_data[data]:
-        if key == 'poi' and enron_data[data]['poi'] == 1:
-            poiCount += 1;
+    if enron_data[data]["poi"] == 1:
+        poiCount += 1;
 
 print(poiCount)
 
 # how many POIs Exist?
 totalPoi = 0;
 with open('../final_project/poi_names.txt', 'r') as f:
-    file_data = f.read()
     while (f.readline()):
-        print("reading line")
-        totalPoi += 1;
+        totalPoi += 1
 print(totalPoi)
+
+print(enron_data["PRENTICE JAMES"]["total_stock_value"])
+print(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+print(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+
+totalPayments = [(name, enron_data[name]["total_payments"]) for name in enron_data if
+                 (re.search('^LAY|^SKILLING|^FASTOW', name))]
+# maxKey = max(keyFound)[1]
+# print(maxKey)
+totalPayments.sort(reverse=True, key=lambda a: a[1])
+print(totalPayments[0])
+for name in enron_data:
+    for key in enron_data[name]:
+        print(key)
+    break
+
+knownEmail = [name for name in enron_data if enron_data[name]["email_address"] != 'NaN']
+quantifiedSalary = [name for name in enron_data if enron_data[name]["salary"] != 'NaN']
+print(len(knownEmail))
+print(len(quantifiedSalary))
